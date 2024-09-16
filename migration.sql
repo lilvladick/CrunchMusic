f9a5a39dd6b67ac32a67c02d01e690a2f76a0709
Vladick <vlad2004ki@gmail.com>
2024-09-16 19:26:17 +0300

    new migrations sql

--- migration.sql
+++ migration.sql
@@ -1,19 +1,24 @@
-CREATE TABLE "users" (
-    "id" BIGSERIAL NOT NULL UNIQUE,
-    "name" VARCHAR NOT NULL,
-    "login" VARCHAR NOT NULL UNIQUE,
-    "password" BYTEA NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password BYTEA NOT NULL
 );
 
-CREATE TABLE "tracks" (
-    "id" BIGSERIAL NOT NULL UNIQUE,
-    "title" VARCHAR NOT NULL,
-    "user_id" BIGINT NOT NULL,
-    "genre" VARCHAR NOT NULL,
-    "duration" INTERVAL,
-    "file_path" VARCHAR NOT NULL,  -- Путь к файлу на сервере
-    PRIMARY KEY("id"),
-    FOREIGN KEY("user_id") REFERENCES "users"("id")
-    ON UPDATE NO ACTION ON DELETE CASCADE
+CREATE TABLE tracks (
+    id UUID PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    filepath TEXT NOT NULL,
+    user UUID NOT NULL,
+    genre VARCHAR NOT NULL,
+    duration TIMESTAMP
 );
+
+CREATE TABLE likes (
+    user_id UUID NOT NULL,
+    track_id UUID NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_track_id FOREIGN KEY (track_id) REFERENCES tracks (id)
+);
+
+ALTER TABLE tracks ADD CONSTRAINT fk_user FOREIGN KEY (user) REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
